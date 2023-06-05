3290dfd23f7ddc761c07ab20f02f1630a3d3cb63
XorySh <melchordiazreyes@gmail.com>
2023-06-06 01:30:39 +0200

    Avance en Inserts Incidencias e usuarios, relaciones en tabla TipoIncidencia actualizada

--- incideitor-master/src/main/resources/data.sql
+++ incideitor-master/src/main/resources/data.sql
@@ -1,21 +1,23 @@
-INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', 'Juan', 'Juan', 'Juan', '1', 'Juan', 'Juan');
-INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', 'Juan', 'Juan', 'Juan', '1', 'Juan', 'Juan');
-INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', 'Juan', 'Juan', 'Juan', '1', 'Juan', 'Juan');
-INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', 'Juan', 'Juan', 'Juan', '1', 'Juan', 'Juan');
-INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', 'Juan', 'Juan', 'Juan', '1', 'Juan', 'Juan');
-INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', 'Juan', 'Juan', 'Juan', '1', 'Juan', 'Juan');
-INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', 'Juan', 'Juan', 'Juan', '1', 'Juan', 'Juan');
+INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('Juan', 'Y medio', 'Juan', 'mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', '1', 'Juan', 'Juan');
+INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('Pepe', 'Caja', 'Juan', 'mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', '1', 'Juan', 'Juan');
+INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('Manolo', 'Bombo', 'Juan', 'mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', '1', 'Juan', 'Juan');
+INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('Messi', 'Dios', 'Juan', 'mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', '1', 'Juan', 'Juan');
+INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('Guardiola', 'Juan', 'Juan', 'mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', '1', 'Juan', 'Juan');
+INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('Rambo', 'Ak', 'Juan', 'mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', '1', 'Juan', 'Juan');
+INSERT INTO usuarios (nombre, apellido, avatar, email, dni, contrasena, sexo, departamento, extension) VALUES ('Shrek', 'Gordo', 'Juan', 'mail.alejandro.teixeira@gmail.com', 'Juan', 'Juan', '1', 'Juan', 'Juan');
 
-INSERT INTO incidencias(id, titulo, descripcion, fecha) VALUES ('1','Apocalipsis','Ha llegado Rambo con un ak-47','2023-06-01');
-INSERT INTO incidencias(id, titulo, descripcion, fecha) VALUES ('2','Apocalipsis','Ha llegado Rambo con un ak-47','2023-06-01');
-INSERT INTO incidencias(id, titulo, descripcion, fecha) VALUES ('3','Apocalipsis','Ha llegado Rambo con un ak-47','2023-06-01');
-INSERT INTO incidencias(id, titulo, descripcion, fecha) VALUES ('4','Apocalipsis','Ha llegado Rambo con un ak-47','2023-06-01');
+INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('Chiclana', 'Juan', 'Juan', 'Juan', 'Juan');
+INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('Jerez', 'Juan', 'Juan', 'Juan', 'Juan');
+INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('Murcia', 'Juan', 'Juan', 'Juan', 'Juan');
+INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('Albacete', 'Juan', 'Juan', 'Juan', 'Juan');
+INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('Toledo', 'Juan', 'Juan', 'Juan', 'Juan');
+INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('Miami', 'Juan', 'Juan', 'Juan', 'Juan');
 
-INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('alejandro', 'Juan', 'Juan', 'Juan', 'Juan');
-INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('alejandro', 'Juan', 'Juan', 'Juan', 'Juan');
-INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('alejandro', 'Juan', 'Juan', 'Juan', 'Juan');
-INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('alejandro', 'Juan', 'Juan', 'Juan', 'Juan');
-INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('alejandro', 'Juan', 'Juan', 'Juan', 'Juan');
-INSERT INTO ayuntamientos (nombre, fotoCabecera, fotologin, foto3, foto4) VALUES ('alejandro', 'Juan', 'Juan', 'Juan', 'Juan');
+INSERT INTO incidencias(titulo, descripcion, fecha, usuario_id, ayuntamiento_id) VALUES ('Apocalipsis','Ha llegado Rambo con un ak-47','2023-06-01',1,1);
+INSERT INTO incidencias(titulo, descripcion, fecha, usuario_id, ayuntamiento_id) VALUES ('Afilador','Ha llegado el afilador','2023-06-01',2,3);
+INSERT INTO incidencias(titulo, descripcion, fecha, usuario_id, ayuntamiento_id) VALUES ('Churreria','Problema en Churreria Hmnos Pernia','2023-06-01',3,4);
+INSERT INTO incidencias(titulo, descripcion, fecha, usuario_id, ayuntamiento_id) VALUES ('Guerra nuclear','Y hizo pum ya esta aqui la guerra','2023-06-01',4,5);
+
+
 
 INSERT INTO reportes (categoria, descripcion) VALUES ('0', 'No se ve la pantalla')
