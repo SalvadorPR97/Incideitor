fca43945c26627eb0e68b87304a7188bf5bf21ab
XorySh <melchordiazreyes@gmail.com>
2023-06-20 20:22:59 +0200

    Comentado ObtenerDatosUsuario y añadidas las imagenes de avatar

--- incideitor-master/src/main/java/com/example/eoi/incideitor/util/ObtenerDatosUsuario.java
+++ incideitor-master/src/main/java/com/example/eoi/incideitor/util/ObtenerDatosUsuario.java
@@ -16,22 +16,30 @@ public class ObtenerDatosUsuario {
     @Autowired
      UsuarioRepository usuarioRepository;
 
+    //Metodo para obtener los datos del usuario que se encuentra actualmente autenticado
     public Usuario getUserData(){
         String  userName = "no informado";
+
+        //Se imprime en la consola el resultado de llamar al método toString para ver que informacion obtenemos del usuario
         System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+        //Creamos un usuario vacío
         Usuario usuario = new Usuario();
-        //Comprobamos si hay usuario logeado
+        //Comprobamos si hay usuario autenticado, comparando con la cadena "anonymosUser", si es igual asignamos el siguiente userName
         if (SecurityContextHolder.getContext().getAuthentication().getPrincipal().equals("anonymousUser")){
             userName = "anonimo@anonimo.com";
         } else {
+            //Si se encuentra un usuario autenticado, distinto a "anonimousUser", obtenemos su nombre de usuario y lo asignamos a la variable
             userName = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
             //Obtenemos datos del usuario
         }
+        //Buscamos un usuario en la base de datos correspondiente al nombre de "userName"
         Optional<Usuario> usuarioOptional = usuarioRepository.findUsuarioByEmail(userName);
+        //Si se encuentra, se le asigna al objeto usuario el usuario encontrado
         if (usuarioOptional.isPresent()){
             usuario = usuarioOptional.get();
         }
 
+        //Devolvemos el objeto usuario
         return usuario;
     }
 }
